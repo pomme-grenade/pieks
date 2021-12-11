586af8893563e0d9885fe23de4827f82e7bdacdc
Patrice <patrice.epplee@gmail.com>
2021-12-11 21:32:15 +0100

    calculate game in server

--- backend/main.py
+++ backend/main.py
@@ -24,6 +24,7 @@ class Game:
         self.draw_cards(p1)
         self.draw_cards(p2)
         self.current_player = p1
+        self.last_action = None
 
     async def start_game(self):
         # await self.p1.con.send_text("los")
@@ -50,16 +51,34 @@ class Game:
            "own_pos": own_player.pos,
            "other_pos": own_player.other_player.pos,
            "current_player": jsonable_encoder(self.current_player.id),
-           "last_action": None,
+           "last_action": self.last_action,
            "own_hand": own_player.hand,
            "other_hand": own_player.other_player.hand,
-
        }
+
+    async def update_state(self, data, id):
+        player = self.get_player_by_id(id)
+        if data["action"] == "moveRight":
+            player.pos += data["cards"][0]
+        elif data["action"] == "moveLeft":
+            player.pos -= data["cards"][0]
+
+        self.current_player = player.other_player
+        self.last_action = data["action"]
+
+        await self.p1.con.send_json(self.get_state(self.p1))
+        await self.p2.con.send_json(self.get_state(self.p2))
+
+    def get_player_by_id(self, id):
+       if id == self.p1.id:
+           return self.p1
+       else:
+           return self.p2
 
 class ConnectionManager:
     def __init__(self):
         self.queue: Optional[Player] = None
-        self.games: List[Game] = []
+        self.games = {}
 
     async def connect(self, websocket: WebSocket, id: UUID):
         await websocket.accept()
@@ -72,8 +91,9 @@ class ConnectionManager:
             new_player = Player(websocket, id, 0, self.queue)
             self.queue.other_player = new_player
             game = Game(self.queue, new_player)
+            self.games[self.queue.id] = game
+            self.games[new_player.id] = game
             self.queue = None
-            self.games.append(game)
             await game.start_game()
             print("created game")
 
@@ -89,6 +109,8 @@ class ConnectionManager:
         # for connection in self.queue:
             # await connection.send_text(message)
 
+    
+
 
 manager = ConnectionManager()
 
@@ -97,8 +119,8 @@ async def websocket_endpoint(websocket: WebSocket, client_id: UUID):
     await manager.connect(websocket, client_id)
     try:
         while True:
-            data = await websocket.receive_text()
-            print(data)
+            data = await websocket.receive_json()
+            await manager.games[client_id].update_state(data, client_id)
             # await manager.send_personal_message(f"You wrote: {data}", websocket)
     except WebSocketDisconnect:
         manager.disconnect(websocket)
